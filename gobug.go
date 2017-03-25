64e6f309d74f4384bd04407246439147caa2dae7
philipkavanagh.com <philkav1989@gmail.com>
2017-03-25 12:02:39 +0000

    Added this

--- gobug.go
+++ gobug.go
@@ -1,7 +1,20 @@
 package main
 
-import fmt
+import ( "fmt" )
+
+func header() string {
+	return "gobug (Bug tracking system) by Philip Kavanagh";
+}
+
+func help() string {
+	return	` 
+	-a SUBJECT : adds a bug with the provided subject
+	-l         : lists bugs
+	-c BUGID   : closes a bug defined by the bugid
+		`
+}
 
 func main(){
-	fmt.Println("gobug (Bug tracking system) by PHilip Kavanagh");
+	fmt.Println(header());
+	fmt.Println(help());
 }
